679d241ef11afc79b2d56956990d706cf0573146
Kwag De Gi <tech@skyaperture.com>
2015-01-31 09:18:00 +0900

    const

--- 0131/index.php
+++ 0131/index.php
@@ -1,5 +1,5 @@
 <?php
-
+define(endl, "\n");
 $array = [1,2,3,4,5];
 
 $end = end($array);
@@ -7,3 +7,5 @@ $end = end($array);
 foreach ( $array as $el ) {
 	echo $el . "\n";
 }
+
+echo $end . endl;
